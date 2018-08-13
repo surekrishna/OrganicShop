1acd4a1636640ed291837b99e5e512fa8d30397a
Krishna <surekrrishna@gmail.com>
2018-08-13 10:52:19 -0400

    displaying all the products @home page

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ProductService } from '../product.service';
+import { Product } from '../models/product';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
-export class ProductsComponent implements OnInit {
+export class ProductsComponent implements OnDestroy {
 
-  constructor() { }
+  productList: Product[]; 
+  subscription: Subscription; 
+  
+  constructor(private productService: ProductService) { 
+    this.productList =  this.getProductList();    
+  }
 
-  ngOnInit() {
+  getProductList(){    
+    this.productList = [];
+    this.subscription = this.productService.getAllProducts().snapshotChanges().subscribe(products =>{        
+      products.forEach(product =>{
+        let jProduct = product.payload.toJSON();
+        jProduct['$key'] = product.key;
+        this.productList.push(jProduct as Product);        
+      });
+    });
+    
+    return this.productList;     
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 
 }
