4e964f71e3fd8301f8dbabba132d0bb2c5798ad8
Krishna <surekrrishna@gmail.com>
2018-08-13 11:03:13 -0400

    displaying categories @home component

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ProductService } from '../product.service';
 import { Product } from '../models/product';
 import { Subscription } from 'rxjs';
+import { Category } from '../models/category';
 
 @Component({
   selector: 'app-products',
@@ -12,9 +13,11 @@ export class ProductsComponent implements OnDestroy {
 
   productList: Product[]; 
   subscription: Subscription; 
+  categoryList: Category[];
   
   constructor(private productService: ProductService) { 
-    this.productList =  this.getProductList();    
+    this.productList =  this.getProductList(); 
+    this.categoryList = this.getCategories();   
   }
 
   getProductList(){    
@@ -28,6 +31,18 @@ export class ProductsComponent implements OnDestroy {
     });
     
     return this.productList;     
+  }
+
+  getCategories(){   
+    this.categoryList = [];
+    this.subscription =  this.productService.getCategories().snapshotChanges().subscribe(categories =>{
+      categories.forEach(category =>{              
+        let jCategory = category.payload.toJSON();          
+        jCategory['key'] = category.payload.key;                        
+        this.categoryList.push(jCategory as Category);
+      });
+    });
+    return this.categoryList;
   }
 
   ngOnDestroy(): void {
