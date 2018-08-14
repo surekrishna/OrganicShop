2dad327f471a0eee83d4256ac041dd8ac7425e53
Krishna <surekrrishna@gmail.com>
2018-08-14 05:26:47 -0400

    created shopping cart

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { Product } from '../models/product';
+import { ShoppingCartService } from '../shopping-cart.service';
 
 @Component({
   selector: 'product-card',
@@ -11,7 +12,16 @@ export class ProductCardComponent {
   @Input('product') product: Product;
   @Input('show-actions') showActions = true;
 
-  constructor() { }
+  constructor(private cartService: ShoppingCartService) { }
+
+  addToCart(product: Product){
+    let cartId = localStorage.getItem('cartId');
+    if(!cartId){
+      this.cartService.createCart().then(result =>{        
+        localStorage.setItem('cartId', result.key);
+      });
+    }
+  }
 
  
 
---
+++ src/app/shopping-cart.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { AngularFireDatabase } from 'angularfire2/database';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ShoppingCartService {
+
+  constructor(private db: AngularFireDatabase) { }
+
+  createCart(){
+    return this.db.list('shopping-cart').push({
+        dateCreated: new Date().getTime()
+    });
+  }
+}
