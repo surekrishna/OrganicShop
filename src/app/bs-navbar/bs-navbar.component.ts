3c8cc64a962df60190d934291106bf15c0da9ec7
Krishna <surekrrishna@gmail.com>
2018-08-21 09:25:16 -0400

    displaying the number of shopping cartitems in navbar component and created models

--- src/app/bs-navbar/bs-navbar.component.ts
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -1,20 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth.service';
 import { AppUser } from '../models/app-user';
+import { ShoppingCartService } from '../shopping-cart.service';
+import { ShoppingCart } from '../models/shopping-cart';
+
 
 @Component({
   selector: 'bs-navbar',
   templateUrl: './bs-navbar.component.html',
   styleUrls: ['./bs-navbar.component.css']
 })
-export class BsNavbarComponent {
+export class BsNavbarComponent implements OnInit {
+  
   appUser: AppUser;
-  constructor(private auth: AuthService) { 
-    this.auth.appUser$.subscribe(appUser => this.appUser = appUser);
+  shoppingCartItemCount: number;  
+
+  constructor(private auth: AuthService, private cartService: ShoppingCartService) {     
   }
 
   logout(){
     this.auth.logout();
+  }
+
+  ngOnInit(){
+    this.auth.appUser$.subscribe(appUser => this.appUser = appUser);
+            
+    this.cartService.getCart().snapshotChanges().subscribe(cart =>{      
+      this.shoppingCartItemCount = 0;  
+      let cartItems = cart.payload.toJSON() as ShoppingCart;      
+        for(let productID in cartItems.items){        
+          this.shoppingCartItemCount += cartItems.items[productID].quantity;          
+        }                   
+    });    
   }
 
 }
