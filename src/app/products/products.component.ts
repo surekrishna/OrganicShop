46b22ff4794278eb711c6186a4262d77426a39fd
Krishna <surekrrishna@gmail.com>
2018-08-22 14:07:28 -0400

    code refactored

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -21,9 +21,7 @@ export class ProductsComponent implements OnInit, OnDestroy {
   
   constructor(private productService: ProductService, 
               private route: ActivatedRoute,
-              private cartService: ShoppingCartService) { 
-    this.productList =  this.getProductList();         
-  }
+              private cartService: ShoppingCartService) { }
 
   getProductList(){    
     this.productList = [];
@@ -37,9 +35,7 @@ export class ProductsComponent implements OnInit, OnDestroy {
         this.route.queryParamMap.subscribe(params =>{
           this.category = params.get('category');
           
-          this.filteredProducts = (this.category) 
-          ? this.productList.filter(product => product.category == this.category) 
-          : this.productList;
+          this.applyFilter();
         }); 
       });
     });
@@ -47,10 +43,17 @@ export class ProductsComponent implements OnInit, OnDestroy {
     return this.productList;     
   }
 
+  private applyFilter(){
+    this.filteredProducts = (this.category) 
+    ? this.productList.filter(product => product.category == this.category) 
+    : this.productList;
+  }
+
   ngOnInit(){
     this.subscription = this.cartService.getCart().snapshotChanges().subscribe(cart =>{
         this.cart = cart;        
-    });
+    });    
+    this.productList =  this.getProductList();  
   }
 
   ngOnDestroy(): void {
