34176648ddda79f631a0a4cb339a2f1fadfcd058
Krishna <surekrrishna@gmail.com>
2018-08-27 05:23:51 -0400

    created shared module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,37 +1,32 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AngularFireModule } from 'angularfire2';
-import { AngularFireDatabaseModule } from 'angularfire2/database';
-import { AngularFireAuthModule } from 'angularfire2/auth';
-import { RouterModule, Router } from '@angular/router';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { CustomFormsModule } from 'ng2-validation';
+import { AuthGuard } from 'shared/services/auth-guard.service';
 
-import { AppComponent } from './app.component';
 import { environment } from '../environments/environment';
+import { AdminAuthGuard } from './admin-auth-guard.service';
+import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
+import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
+import { ProductFormComponent } from './admin/product-form/product-form.component';
+import { AppComponent } from './app.component';
 import { BsNavbarComponent } from './bs-navbar/bs-navbar.component';
-import { HomeComponent } from './home/home.component';
-import { ProductsComponent } from './products/products.component';
-import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 import { CheckOutComponent } from './check-out/check-out.component';
-import { OrderSuccessComponent } from './order-success/order-success.component';
+import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { MyOrdersComponent } from './my-orders/my-orders.component';
-import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
-import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
-import { AuthService } from 'shared/services/auth.service';
-import { AuthGuard } from 'shared/services/auth-guard.service';
-import { UserService } from 'shared/services/user.service';
-import { AdminAuthGuard } from './admin-auth-guard.service';
-import { ProductFormComponent } from './admin/product-form/product-form.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { CustomFormsModule } from 'ng2-validation';
-import { ProductService } from 'shared/services/product.service';
+import { OrderSuccessComponent } from './order-success/order-success.component';
 import { ProductFilterComponent } from './products/product-filter/product-filter.component';
-import { ProductCardComponent } from 'shared/components/product-card/product-card.component';
-import { ProductQuantityComponent } from 'shared/components/product-quantity/product-quantity.component';
-import { OrderService } from 'shared/services/order.service';
+import { ProductsComponent } from './products/products.component';
 import { ShippingCartSummaryComponent } from './shipping-cart-summary/shipping-cart-summary.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { SharedModule } from 'shared/shared.module';
 
 
 @NgModule({
@@ -48,13 +43,12 @@ import { ShippingCartSummaryComponent } from './shipping-cart-summary/shipping-c
     AdminProductsComponent,
     AdminOrdersComponent,
     ProductFormComponent,
-    ProductFilterComponent,
-    ProductCardComponent,
-    ProductQuantityComponent,
+    ProductFilterComponent,    
     ShippingCartSummaryComponent
   ],
   imports: [  
     FormsModule,
+    SharedModule,
     ReactiveFormsModule,   
     CustomFormsModule,  
     BrowserModule,
@@ -82,12 +76,7 @@ import { ShippingCartSummaryComponent } from './shipping-cart-summary/shipping-c
     ])
   ],
   providers: [
-    AuthService,
-    AuthGuard,
-    AdminAuthGuard,
-    UserService,
-    ProductService,
-    OrderService    
+    AdminAuthGuard,    
   ],
   bootstrap: [AppComponent]
 })
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,31 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { ProductCardComponent } from 'shared/components/product-card/product-card.component';
+import { ProductQuantityComponent } from 'shared/components/product-quantity/product-quantity.component';
+import { AuthGuard } from 'shared/services/auth-guard.service';
+import { AuthService } from 'shared/services/auth.service';
+import { OrderService } from 'shared/services/order.service';
+import { ProductService } from 'shared/services/product.service';
+import { UserService } from 'shared/services/user.service';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [
+    ProductCardComponent,
+    ProductQuantityComponent,
+  ],
+  exports: [
+    ProductCardComponent,
+    ProductQuantityComponent,
+  ],
+  providers: [
+    AuthService,
+    AuthGuard,   
+    UserService,
+    ProductService,
+    OrderService    
+  ]
+})
+export class SharedModule { }
