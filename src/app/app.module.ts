f5d9bca858c12618d5cbea85de362c6db7a52e0c
Krishna <surekrrishna@gmail.com>
2018-08-27 11:47:10 -0400

    created core module and importing and exporting modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,46 +1,32 @@
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
-import { CustomFormsModule } from 'ng2-validation';
 import { SharedModule } from 'shared/shared.module';
 
 import { environment } from '../environments/environment';
 import { AdminModule } from './admin/admin.module';
 import { AdminAuthGuard } from './admin/services/admin-auth-guard.service';
 import { AppComponent } from './app.component';
-import { BsNavbarComponent } from './bs-navbar/bs-navbar.component';
-import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './core/components/login/login.component';
+import { CoreModule } from './core/core.module';
 import { ProductsComponent } from './shopping/components/products/products.component';
 import { ShoppingModule } from './shopping/shopping.module';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    BsNavbarComponent,
-    HomeComponent,  
-    LoginComponent,   
+    AppComponent    
   ],
-  imports: [  
-    FormsModule,
+  imports: [      
     ShoppingModule,
     SharedModule,
-    AdminModule,    
-    ReactiveFormsModule,   
-    CustomFormsModule,  
-    BrowserModule,
-    BrowserAnimationsModule, 
+    AdminModule, 
+    CoreModule,                   
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireDatabaseModule,
-    AngularFireAuthModule,
-    NgbModule.forRoot(),       
+    AngularFireAuthModule,       
     RouterModule.forRoot([
       {path: '', component: ProductsComponent},      
       {path: 'login', component: LoginComponent},          
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,5 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CustomFormsModule } from 'ng2-validation';
 import { ProductCardComponent } from 'shared/components/product-card/product-card.component';
 import { ProductQuantityComponent } from 'shared/components/product-quantity/product-quantity.component';
 import { AuthGuard } from 'shared/services/auth-guard.service';
@@ -7,10 +9,21 @@ import { AuthService } from 'shared/services/auth.service';
 import { OrderService } from 'shared/services/order.service';
 import { ProductService } from 'shared/services/product.service';
 import { UserService } from 'shared/services/user.service';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    NgbModule.forRoot(),    
+    BrowserModule,
+    BrowserAnimationsModule, 
+    FormsModule,    
+    CustomFormsModule,
+     //It looks like you're using ngModel (ReactiveFormsModule) on the same form field as formControlName. Support for using the ngModel input property and ngModelChange event with reactive form directives has been deprecated in Angular v6 and will be removed in Angular v7
+    //For more information on this, see our API docs here: https://angular.io/api/forms/FormControlName#use-with-ngmodel
+    ReactiveFormsModule.withConfig({warnOnNgModelWithFormControl: 'never'}),
   ],
   declarations: [
     ProductCardComponent,
@@ -19,6 +32,12 @@ import { UserService } from 'shared/services/user.service';
   exports: [
     ProductCardComponent,
     ProductQuantityComponent,
+    FormsModule,
+    CustomFormsModule,
+    ReactiveFormsModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    NgbModule,    
   ],
   providers: [
     AuthService,
